b93bf80bc4af443fed8bd3e29c22c2c7527c48c9
Sebastian the Developer <sebastianorellana524@gmail.com>
2019-03-23 14:21:42 -0400

    Added gallery and updated event info

--- src/components/events/events-list.js
+++ src/components/events/events-list.js
@@ -3,7 +3,7 @@ export default [
   // JGM Soccer Tournament
   {
     id: 0,
-    title: 'JGM Memorial Soccer Tournament',
+    title: 'JGM Memorial Soccer Tournament 2019',
     date: {
       dayOfWeek: 'Saturday',
       month: 'June',
@@ -18,7 +18,7 @@ export default [
     },
     time: '8:00am',
     isFree: false,
-    cost: '15.00 per player',
+    cost: '20.00 per player',
     location: {
       name: 'Fred Wolfe Soccer Park',
       street: '300 Hollow Road',
@@ -27,10 +27,11 @@ export default [
       zip: '06477',
     },
     description: 'The JGM Memorial Soccer Tournament is an annual soccer competition held in honor of the late Jacob Gibbons-Morales. Throughout his life, Jacob’s insatiable love for soccer brought together hundreds of people from different cultures and backgrounds by organizing numerous events. From local pick-up games in Connecticut to transporting groups of people to professional games both domestic, as well as on the international stage, Jacob spread his passion for the game throughout the United States as well as abroad. With this honorary tournament, we aim to carry on Jacob’s spirit and mission to bridge cultural barriers.',
-    summary: 'The JGM Memorial Soccer Tournament is an annual soccer competition held in honor of the late Jacob Gibbons-Morales. Teams can register by following the regsitration link. T-shirt, lunch and refreshments will be provided. Registration ends: May 15, 2019',
+    summary: 'The JGM Memorial Soccer Tournament is an annual soccer competition held in honor of the late Jacob Gibbons-Morales. Teams can register by following the registration link. T-shirt, lunch and water will be provided. Concessions will be available. Registration ends: May 15, 2019',
     page: '/jgm-soccer-tournament',
     registration: true,
     registrationLink: '/jgm-soccer-tournament/registration',
+    registrationDeadline: 'May 15, 2019',
     socialMedia: true,
     socials: [
       {
@@ -45,6 +46,50 @@ export default [
         link: 'JGMT-Ball.jpg',
         lazy: '',
         alt: 'image of soccer ball',
+      },
+    ],
+    gallery: [
+      {
+        id: '1',
+        link: 'JGM2018-6.jpg',
+        lazy: '',
+        alt: 'family',
+      },
+      {
+        id: '2',
+        link: 'JGM2018-5.jpg',
+        lazy: '',
+        alt: 'event 2018',
+      },
+      {
+        id: '3',
+        link: 'JGM2018-1.jpg',
+        lazy: '',
+        alt: 'event 2018',
+      },
+      {
+        id: '4',
+        link: 'JGM2018-4.jpg',
+        lazy: '',
+        alt: 'event 2018',
+      },
+      {
+        id: '5',
+        link: 'JGM2018-2.jpg',
+        lazy: '',
+        alt: 'event 2018',
+      },
+      {
+        id: '6',
+        link: 'JGM2018-3.jpg',
+        lazy: '',
+        alt: 'event 2018',
+      },
+      {
+        id: '7',
+        link: 'JGM2018-7.jpg',
+        lazy: '',
+        alt: 'event 2018',
       },
     ],
   },
