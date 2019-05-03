20de3ad81f489d19dd56f4f6126a06596bd977e3
Sebastian the Developer <sebastianorellana524@gmail.com>
2019-05-02 20:23:14 -0400

    cleaned up boiler plate

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import Vue from 'vue';
